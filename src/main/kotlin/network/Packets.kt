455a7ec44cc0cffe9b7be6370be92da6925a9612
BarrosoK <37458477+BarrosoK@users.noreply.github.com>
2018-07-23 21:55:05 +0200

    Update Packets.kt

--- src/main/kotlin/network/Packets.kt
+++ src/main/kotlin/network/Packets.kt
@@ -5,9 +5,9 @@ import network.server.*
 // [XXX XXXXXXXX\n]
 // 100 Kevin
 enum class Packets(val value: Int, val packet: Packet) {
-    REQUEST_LOGIN(1, RequestLogin()),
+    	REQUEST_LOGIN(1, RequestLogin()),
 	REQUEST_CHAR_LOBBY(3, RequestSelectionCharacter()),
 	REQUEST_CHAR_INFO(4, RequestCharInfo()),
-    REQUEST_PLAYER_LIST(100, RequestPlayerList()),
-    REQUEST_CHANNELS_LIST(101, RequestChannelList())
+    	REQUEST_PLAYER_LIST(100, RequestPlayerList()),
+    	REQUEST_CHANNELS_LIST(101, RequestChannelList())
 }
